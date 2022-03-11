cb8ae4005e3c113e0ba5ddb3acb6c2342a1cc001
GuillermoRamon <91343468+GuillermoRamon@users.noreply.github.com>
2022-03-11 17:31:03 +0100

    models

--- almacenes/models/models.py
+++ almacenes/models/models.py
@@ -19,3 +19,22 @@
 from odoo import models ,fields, api, exceptions
 from datetime import date
 from dateutil.relativedelta import *
+
+class almacenes(models.Model):
+    _name = 'almacenes.almacenes'
+    _description = 'Define los atributos'
+
+    #Atributos
+    nombreAlm = fields.Char(string='Nombre del almacen')
+    direccionAlm = fields.Char(string='Direccion')
+
+
+class productos(models.Model):
+    _name = 'almacenes.productos'
+    _description = 'Define los atributos'
+
+    #Atributos
+    nombreProd = fields.Char(string='Nombre del producto')
+    tipoProd = fields.Selection(string='Tipo de producto', selection=[('a','fresco'),('b','congelado'),('c','enlatado'),('d','empaquetado')], help='Estado del alimento')
+    cantidadProd = fields.Integer(string='Cantidad')
+    descripcionProd = fields.Text(string='Descripcion del producto')
--- proveedores/models/models.py
+++ proveedores/models/models.py
@@ -19,3 +19,21 @@
 from odoo import models ,fields, api, exceptions
 from datetime import date
 from dateutil.relativedelta import *
+
+class proveedores(models.Model):
+    _name = 'proveedores.proveedores'
+    _description = 'Define los atributos'
+
+    #Atributos
+    dniSupp = fields.Char(string='DNI', required=True)
+    nombreSupp = fields.Char(string='Nombre del proveedor')
+    telefonoSupp = fields.Char(string='Telefono')
+    emailSupp = fields.Char(string='Email')
+
+class repartos(models.Model):
+    _name = 'proveedores.repartos'
+    _description = 'Define los atributos'
+
+    #Atributos
+    cod = fields.Char(string='Codigo')
+    fecha = fields.Date(string='Fecha de reparto', default = fields.date.today())
