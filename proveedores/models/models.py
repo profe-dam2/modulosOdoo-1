fb49a11b90078db05d58d51afc8daad6b0af3468
GuillermoRamon <91343468+GuillermoRamon@users.noreply.github.com>
2022-03-12 12:34:13 +0100

    validacion error

--- proveedores/models/models.py
+++ proveedores/models/models.py
@@ -32,11 +32,11 @@ class proveedores(models.Model):
     
     @api.constrains('dniSupp')
     def _checkdni(self):
-		for proveedores in self:
-			if(len(proveedores.dniSupp) > 9):
-			    raise exceptions.ValidationError("El DNI no puede ser superior a 9 caracteres")
+        for proveedores in self:
+            if(len(proveedores.dniSupp) > 9):
+                raise exceptions.ValidationError("El DNI no puede ser superior a 9 caracteres")
                 if(len(proveedores.dniSupp) < 9):
-				    raise exceptions.ValidationError("El DNI no puede tener menos de 9 caracteres")
+                    raise exceptions.ValidationError("El DNI no puede tener menos de 9 caracteres")
 
 class repartos(models.Model):
     _name = 'proveedores.repartos'
@@ -47,10 +47,10 @@ class repartos(models.Model):
     fecha = fields.Date(string='Fecha de reparto', default = fields.date.today())
 
     #@api.constrains('fecha')
-	#def _checkFecha(self):
-	#	hoy = date.today()
-	#	for proyecto in self:
-	#		repartos.fecha
-	#		dias = relativedelta(hoy, repartos.fecha).days
-	#		if(dias > 0):
-	#			raise exceptions.ValidationError("Error, la fecha del reparto no puede ser anterior a la fecha actual")
+    #def _checkFecha(self):
+    #	hoy = date.today()
+    #	for proyecto in self:
+    #		repartos.fecha
+    #		dias = relativedelta(hoy, repartos.fecha).days
+    #		if(dias > 0):
+    #			raise exceptions.ValidationError("Error, la fecha del reparto no puede ser anterior a la fecha actual")
