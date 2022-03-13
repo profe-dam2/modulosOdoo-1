e42cb1d060805acef1b9b5e3eea28b95a37b64a5
GuillermoRamon <91343468+GuillermoRamon@users.noreply.github.com>
2022-03-13 16:56:52 +0100

    relaciones de tablas

--- almacenes/models/models.py
+++ almacenes/models/models.py
@@ -31,7 +31,7 @@ class almacen(models.Model):
 
     #Relacion entre tablas
     productos_codProds = fields.Many2many('almacenes.productos', string='Productos')
-    reparto_alm = fields.One2many('proveedores.repartos', 'alm_rep')
+    reparto_alm = fields.Many2one('proveedores.repartos', 'alm_rep')
 
 
 class productos(models.Model):
@@ -63,4 +63,4 @@ class productos(models.Model):
     
     #Relacion entre tablas
     almacenes_codAlms = fields.Many2many('almacenes.almacen', string='Almacenes')
-    reparto_pro = fields.One2many('proveedores.repartos', 'producto_rep')
+    reparto_pro = fields.Many2one('proveedores.repartos', 'producto_rep')
